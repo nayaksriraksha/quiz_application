2b9a9e41f8de5999ad8d346127897b7b222fb932
sriraksha <sriraksha.nayak@niveussolutions.com>
2023-02-07 15:19:05 +0530

    First commit

---
+++ lib/services/question_anwer_list.dart
@@ -0,0 +1,52 @@
+Map<int, String> Questions = {
+  1:'Flutter is developed by?',
+  2:'Is Flutter a Programming language?',
+  3:'How many types of widgets are there in Flutter?'
+};
+
+Map<int, String> OptionsForQ1 = {
+  0: '1.  Facebook',
+  1: '2.  Microsoft',
+  2: '3.  Google',
+  3:'4.  IBM',
+};
+
+
+Map<int, bool> OptionsForQ1Answer = {
+  0: false,
+  1: false,
+  2: true,
+  3: false,
+};
+
+
+Map<int, String> OptionsForQ2 = {
+  0: '1.  No',
+  1: '2.  Yes',
+  2: '3.  May be',
+  3:'4.  Some time',
+};
+
+
+Map<int, bool> OptionsForQ2Answer = {
+  0: true,
+  1: false,
+  2: false,
+  3: false,
+};
+
+
+Map<int, String> OptionsForQ3 = {
+  0: '1.  5',
+  1: '2.  3',
+  2: '3.  1',
+  3: '4.  2',
+};
+
+
+Map<int, bool> OptionsForQ3Answer = {
+  0: false,
+  1: false,
+  2: false,
+  3: true,
+};
